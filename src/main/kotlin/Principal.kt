676876a792503db585e980e5ae20c103935500b2
Darian Hiebl <dahiebl@gmail.com>
2023-11-14 14:42:29 -0300

    agregadas pruebas

--- src/main/kotlin/Principal.kt
+++ src/main/kotlin/Principal.kt
@@ -2,7 +2,7 @@
     fun main(){
         val tp1 = Tp1()
         val texto = Texto()
-        val FRASE = "Aprendi a programar mi primer codigo en kotlin"
+        val FRASE = "Aprendi a escribir mis primeras lineas en kotlin"
         val PALABRA = "kotlin"
         println(tp1.algoritmo(6))
         println(texto.cuentaPalabras(FRASE, PALABRA))
